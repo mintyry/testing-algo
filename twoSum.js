8c2c2db77dac05e66097b4bb974bdb819fa37605
mintyry <gitmintyry@gmail.com>
2024-05-07 18:05:42 -0700

    case 1 solved

--- twoSum.js
+++ twoSum.js
@@ -1,5 +1,18 @@
-var twoSum = function(nums, target) {
-    if (nums[0] + nums[1] === target){
-        return [nums.indexOf(nums[0]), nums.indexOf(nums[1])];
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+var twoSum = function (nums, target) {
+    // hardcoded works for this case
+    // if (nums[0] + nums[1] === target){
+    //     return [nums.indexOf(nums[0]), nums.indexOf(nums[1])];
+    // }
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            if (target - nums[i] === nums[j]) {
+                return [nums.indexOf(nums[i]), nums.indexOf(nums[j])]
+            } 
+        }
     }
 };
+
+console.log(twoSum([3, 2, 4], 6))
