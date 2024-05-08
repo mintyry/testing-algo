a5a69fdc104fab22239ec1ff94f8a5f8217e9182
mintyry <gitmintyry@gmail.com>
2024-05-07 17:31:02 -0700

    attempted palindrome with nums

---
+++ palindromeNum.js
@@ -0,0 +1,13 @@
+var isPalindrome = function(x) {
+  
+    return x < 0 ? false : true;
+
+    let
+    while(x > 0){
+
+    }
+
+};
+
+console.log(isPalindrome(-121));
+console.log(isPalindrome(3663));
